b0c040984e8d6a570cc7908cacc77009278152eb
Tim <reich2011@hotmail.de>
2022-12-20 22:44:09 +0000

    v3

--- src/App.svelte
+++ src/App.svelte
@@ -1,12 +1,45 @@
 <script>
     import Nav from './components/nav.svelte';
-    import Button from './components/Button.svelte';
+
     let menuItems = [
         {link: '#', name: 'Docs'},
-        {link: '#', name: 'Home'},
-        {link: '#', name: 'Blog'}
+        {link: '#', name: 'Home'}
     ];
+
+    const apiKey = 'VEQB36CS6FYOPPUO';
+    //const apiKey = '2PD6SXM6VTYLAZ5D';
+
+    let ticker = 'GOOG';
+
+    async function getStockData() {
+        let response = await fetch(
+            'https://www.alphavantage.co/query?function=OVERVIEW&symbol=' + ticker + '&apikey=' + apiKey
+        );
+        let stockData = await response.json();
+        return stockData;
+    }
+    let promise = getStockData();
 </script>
 
 <Nav {menuItems} />
-<Button text="Something" />
+
+<input bind:value={ticker} />
+<button on:click={getStockData}>Click to Load Stock Data</button>
+
+<div>
+    {#await promise}
+        <p>Loading...</p>
+    {:then stockData}
+        <tbody>
+            {#each Object.entries(stockData) as row}
+                <tr>
+                    {#each row as cell}
+                        <td>{cell}</td>
+                    {/each}
+                </tr>
+            {/each}
+        </tbody>
+    {:catch error}
+        <p style="color: red">{error.message}</p>
+    {/await}
+</div>
